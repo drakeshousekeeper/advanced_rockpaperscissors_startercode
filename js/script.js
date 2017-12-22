a34d198da31969bcb3ef7b8d78a807fe4070f8b3
Syndra <syndra.j.kyddhardy@student.uncommonhs.org>
2017-12-22 01:41:18 +0000

    Added if/else statements and variables (currently commented out) to script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,44 @@
-//Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
-//Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
-//Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
-
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
 //GLOBAL VARIABLES
         var userChoice = "";
         var computerChoice = "";
-        var winner = "";
+        var winnerIs = "";
         var choices = ["rock", "paper", "scissors"];
         var num = Math.floor(Math.random()*choices.length);
 
 
-//FUNCTIONS
-//      $("button").click(function(){    
-//              $("#winner").append("---");
-//      });
-// console.log(num);
-// console.log(choices[0]);
-
 // DOCUMENT READY FUNCTION
 // $( document ).ready(function() {
+//      $(".row").hide();
+
+
+// });
+
+
+
+
 //      $("button").click(function(){    
 //              $("#computer").append(val.());
 //              $("#user").append(val.());
 //      });
-// });
+
+
+// var user = $("#user").val();
+// var comp = $("#computer").val();
+// var winn = $("#winner").val();
+
+
+// var user = $("input").val();
+// var comp = $("").val();
+//
+// if (user === "paper" && comp === "rock" || user === "scissors" && comp === "paper" || user === "rock" && comp === "scissors")
+// elseif (user === "rock" && comp === "paper" || user === "paper" && comp === "scissors" || user === "scissors" && comp === "rock")
+// elseif (user === comp)
+// else (user !== "rock" || "paper" || "scissors")
+
+
+
+
+
+
+
+
