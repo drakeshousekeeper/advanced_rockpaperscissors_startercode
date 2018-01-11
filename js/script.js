475f69a02104b347782142e57842efb55f9d160e
Syndra <syndra.j.kyddhardy@student.uncommonhs.org>
2018-01-11 22:46:16 +0000

    IT WORKS THAT IS ALL YOU NEED TO KNOW!

--- js/script.js
+++ js/script.js
@@ -1,45 +1,66 @@
 //GLOBAL VARIABLES
         var choices = ["rock", "paper", "scissors"];
-        var choicesPics = ["<img src='images/rock.png'>", "<img src='images/paper.png'>", "<img src='images/scissors2.png'>"];
-        var results = ["<img src='images/you-win.gif'>", "<img src='images/yo-lose.png'>", "<img src='images/its-a-tie.png'>",]
-        var num = Math.floor(Math.random()*choices.length);
+        var choicesPics = ["<img src='images/rock.png'>", "<img src='images/paper.png'>", "<img src='images/scissors.png'>"];
+        var results = ["<img src='images/you-win.gif'>", "<img src='images/you-lose.png'>", "<img src='images/its-a-tie.png'>",];
 
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-    $(".row").hide();
     
-    $("button").click(function(){
-        var userChoice = $("#user-input").val();
+    $(".row").hide();
+    $("#clear-button").hide();
+    
+    $("#play-button").click(function(){
+        var num = Math.floor(Math.random()*2);
+        var usersInput = $("#user-input").val();
+        var userChoice = usersInput.toLowerCase();
         var compChoice = choices[num];
         
         if (userChoice === "rock") {
-            $("#user").append(choicesPics[0]);
+            $("#user").html(choicesPics[0]);
         } else if(userChoice === "paper") {
-            $("#user").append(choicesPics[1]);
+             $("#user").html(choicesPics[1]);
         } else if(userChoice === "scissors") {
-            $("#user").append(choicesPics[2]);
+            $("#user").html(choicesPics[2]);
+        } else if(userChoice !== "rock" || "paper" || "scissors") {
+            $("#incorrect-input").html("You have to type in an ACTUAL choice to comply with the code properly, man :/");
         }
         
         if (compChoice === "rock") {
-            $("#comp").append(choicesPics[0]);
+            $("#comp").html(choicesPics[0]);
         } else if(compChoice === "paper") {
-            $("#comp").append(choicesPics[1]);
+            $("#comp").html(choicesPics[1]);
         } else if(compChoice === "scissors") {
-            $("#comp").append(choicesPics[2]);
+            $("#comp").html(choicesPics[2]);
         }
         
         if (userChoice === "paper" && compChoice === "rock" || userChoice === "scissors" && compChoice === "paper" || userChoice === "rock" && compChoice === "scissors") {
             $(".row").show();
-            $("#winn").append(results[0]);
+            $("#play-button").hide();
+            $("#clear-button").show();
+            $("#winn").html(results[0]);
+            $("#incorrect-input").empty();
         } else if(userChoice === "rock" && compChoice === "paper" || userChoice === "paper" && compChoice === "scissors" || userChoice === "scissors" && compChoice === "rock") {
             $(".row").show();
-            $("#winn").append(results[1]);
+            $("#play-button").hide();
+            $("#clear-button").show();
+            $("#winn").html(results[1]);
+            $("#incorrect-input").empty();
         } else if(userChoice === compChoice) {
             $(".row").show();
-            $("#winn").append(results[2]);
-        } else if(userChoice !== "rock" || "paper" || "scissors") {
-            $("#incorrect-input").append("Haha, wrong");
+            $("#play-button").hide();
+            $("#clear-button").show();
+            $("#winn").html(results[2]);
+            $("#incorrect-input").empty();
         }
+        
+        $("#clear-button").click(function() {
+            $("#user").html("");
+            $("#comp").html("");
+            $("#winn").html("");
+            $(".row").hide();
+            $("#play-button").show();
+            $("#clear-button").hide();
+        });
     });
 });
