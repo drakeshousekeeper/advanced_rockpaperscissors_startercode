e5a65528d277971bdb528ffa1cd6d192a3594f93
Syndra <syndra.j.kyddhardy@student.uncommonhs.org>
2018-06-25 18:43:54 +0000

    Fixed thumbnail sizes! FINALLY! Added margin-top/bottom to h1, input, buttons and padding to #incorrect-input

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 //GLOBAL VARIABLES
         var choices = ["rock", "paper", "scissors"];
-        var choicesPics = ["<img src='images/rock.png'>", "<img src='images/paper.png'>", "<img src='images/scissors.png'>"];
-        var results = ["<img src='images/you-win.gif'>", "<img src='images/you-lose.png'>", "<img src='images/its-a-tie.png'>",];
+        var choicesPics = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+        var results = ["images/you-win.gif", "images/you-lose.png", "images/its-a-tie.png",];
 
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
     
+    $(".container").hide();
     $(".row").hide();
     $("#clear-button").hide();
+    
+    $("#ready-button").click(function(){
+         $(".container").show();
+         $("#instructions").hide();
+    });
     
     $("#play-button").click(function(){
         var num = Math.floor(Math.random()*2);
@@ -17,47 +23,48 @@ $( document ).ready(function() {
         var compChoice = choices[num];
         
         if (userChoice === "rock") {
-            $("#user").html(choicesPics[0]);
+            $("#user").attr("src", choicesPics[0]);
         } else if(userChoice === "paper") {
-             $("#user").html(choicesPics[1]);
+             $("#user").attr("src", choicesPics[1]);
         } else if(userChoice === "scissors") {
-            $("#user").html(choicesPics[2]);
+            $("#user").attr("src", choicesPics[2]);
         } else if(userChoice !== "rock" || "paper" || "scissors") {
             $("#incorrect-input").html("You have to type in an ACTUAL choice to comply with the code properly, man :/");
+            // $("button").css("margin-bottom", "20px");
         }
         
         if (compChoice === "rock") {
-            $("#comp").html(choicesPics[0]);
+            $("#comp").attr("src", choicesPics[0]);
         } else if(compChoice === "paper") {
-            $("#comp").html(choicesPics[1]);
+            $("#comp").attr("src", choicesPics[1]);
         } else if(compChoice === "scissors") {
-            $("#comp").html(choicesPics[2]);
+            $("#comp").attr("src", choicesPics[2]);
         }
         
         if (userChoice === "paper" && compChoice === "rock" || userChoice === "scissors" && compChoice === "paper" || userChoice === "rock" && compChoice === "scissors") {
             $(".row").show();
             $("#play-button").hide();
             $("#clear-button").show();
-            $("#winn").html(results[0]);
+            $("#winn").attr("src", results[0]);
             $("#incorrect-input").empty();
         } else if(userChoice === "rock" && compChoice === "paper" || userChoice === "paper" && compChoice === "scissors" || userChoice === "scissors" && compChoice === "rock") {
             $(".row").show();
             $("#play-button").hide();
             $("#clear-button").show();
-            $("#winn").html(results[1]);
+            $("#winn").attr("src", results[1]);
             $("#incorrect-input").empty();
         } else if(userChoice === compChoice) {
             $(".row").show();
             $("#play-button").hide();
             $("#clear-button").show();
-            $("#winn").html(results[2]);
+            $("#winn").attr("src", results[2]);
             $("#incorrect-input").empty();
         }
         
         $("#clear-button").click(function() {
-            $("#user").html("");
-            $("#comp").html("");
-            $("#winn").html("");
+            $("#user").attr("src", "");
+            $("#comp").attr("src", "");
+            $("#winn").attr("src", "");
             $(".row").hide();
             $("#play-button").show();
             $("#clear-button").hide();
